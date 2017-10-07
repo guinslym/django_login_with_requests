deb6e6b383e5df746c604d950f255c0413870303
Guinsly Mondésir <guinslym@gmail.com>
2017-10-06 21:39:53 -0400

    with functions

--- scrap.py
+++ scrap.py
@@ -7,25 +7,46 @@ from bs4 import BeautifulSoup
 payload = {
     'next': '/admin/',
     'password':'(famille)',
-    'username':'admin'
+    'username':'admin1'
 }
 
-# Use 'with' to ensure the session context is closed after use.
-with requests.Session() as s:
-    pr = s.get('http://localhost:8000/admin/login/?next=/admin/')
-    # print the html returned or something more intelligent to see if it's a successful login page.
-    soup = BeautifulSoup(pr.content, 'lxml')
+def get_the_csrftoken(webpage):
+    #Parse the content of that page
+    soup = BeautifulSoup(webpage, 'lxml')
+    #Get the CSRF token
     token = soup.find_all('input')[0]['value']
-    payload['csrfmiddlewaretoken'] = token
-    # print (pr.text)
 
-    # An authorised request.
-    import pdb;pdb.set_trace()
-    resp = s.post('http://localhost:8000/admin/login/', data=payload)
-    # print (resp.text)
+    return token
 
-        # etc...
-    f = open("result.html", "w")
-    content = str(resp.content)
-    f.write(content)
-    f.close()
+def get_the_title(webpage):
+    #Parse the content of that page
+    soup = BeautifulSoup(webpage, 'lxml')
+    #Get the CSRF token
+    title = soup.find_all("h1")[1].text
+
+    return title
+
+
+def main():
+    # Use 'with' to ensure the session context is closed after use.
+    with requests.Session() as s:
+        #Go to the login URL page
+        admin_login_page = s.get('http://localhost:8000/admin/login/')
+
+        #Parse the content of that page to retrieve the CSRF Token
+        token = get_the_csrftoken(admin_login_page.content)
+        #Add a new key to the Dictionnary
+        payload['csrfmiddlewaretoken'] = token
+
+        # Get to an authorised request. (that requires Admin)
+        resp = s.post('http://localhost:8000/admin/login/', data=payload)
+
+        title = get_the_title(resp.content)
+
+        #Assert that I receive the right page content
+        assert 'Site administration' in title
+
+
+
+if __name__ == "__main__":
+    main()
